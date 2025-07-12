484abcbb00d4796ad9f5e4a1e54506a7c6bf679f
Nadia Islam <sohasara853@gmail.com>
2025-07-12 16:38:29 +0600

    Add AppBar with title and action buttons to Chatpage

--- lib/features/chat/presentation/chat_page/chatpage.dart
+++ lib/features/chat/presentation/chat_page/chatpage.dart
@@ -5,6 +5,31 @@ class Chatpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chat Page"),
+        backgroundColor: Color(0xFFF5F4FE),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.videocam_sharp, color: Colors.black),
+            onPressed: () {
+              // Implement more options functionality
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.phone, color: Colors.black),
+            onPressed: () {
+              // Implement search functionality
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert, color: Colors.black),
+            onPressed: () {
+              // Implement more options functionality
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
