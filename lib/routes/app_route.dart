9280851d80fa024b15dbf6b5208cdfbe8eb492fe
Nadia Islam <sohasara853@gmail.com>
2025-07-14 16:07:36 +0600

    Add additional routes for chat, contact, and home sections in appRouter

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,5 +1,9 @@
 import 'package:chatting_application/features/chat/presentation/chat_page/chatpage.dart';
 import 'package:chatting_application/features/chat/presentation/home/homePage.dart';
+import 'package:chatting_application/features/chat/presentation/home/personList.dart';
+import 'package:chatting_application/features/contract/presentation/contact_page.dart';
+import 'package:flutter/material.dart';
+
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
@@ -11,9 +15,29 @@ final appRouter = GoRouter(
       builder: (context, state) => const HomePage(),
     ),
     GoRoute(
-      path: '/chat',
-      name: 'chat',
+      path: '/chat-chatpage',
+      name: 'chatpage',
       builder: (context, state) => const Chatpage(),
+    ),
+    GoRoute(
+      path: '/contact-contacts',
+      name: 'contactpage',
+      builder: (context, state) => const ContactPage(),
+    ),
+    GoRoute(
+      path: '/home-allChats',
+      name: 'home-chats',
+      builder: (context, state) => const ChatListScreen(),
+    ),
+    GoRoute(
+      path: '/home-groups',
+      name: 'groups',
+      builder: (context, state) => const Text("Groups Page"),
+    ),
+    GoRoute(
+      path: '/home-contacts',
+      name: 'contacts',
+      builder: (context, state) => const Text("Contacts Page"),
     ),
   ],
 );
