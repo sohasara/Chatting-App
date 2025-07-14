c6b33d319cb26c3257af95d4ccaa198e59fbe719
Nadia Islam <sohasara853@gmail.com>
2025-07-14 17:26:56 +0600

    Comment out Firebase initialization code for future integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import 'package:chatting_application/firebase_options.dart';
+//import 'package:chatting_application/firebase_options.dart';
 import 'package:chatting_application/routes/app_route.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
 }
 
