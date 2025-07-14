ad9bd23690ae0d1398702f09272835dcafb308ae
Nadia Islam <sohasara853@gmail.com>
2025-07-14 10:16:01 +0600

    Update ChatListScreen: Use dynamic personList for item count and names

---
+++ lib/features/chat/data/person_data.dart
@@ -0,0 +1,5 @@
+List personList = [
+  {"name": "Nadia", "image": "", "isOnline": true},
+  {"name": "John", "image": "", "isOnline": false},
+  {"name": "Alice", "image": "", "isOnline": true},
+];
--- lib/features/chat/presentation/home/personList.dart
+++ lib/features/chat/presentation/home/personList.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: file_names
 
+import 'package:chatting_application/features/chat/data/person_data.dart';
 import 'package:flutter/material.dart';
 
 import '../chat_page/chatpage.dart';
@@ -10,13 +11,13 @@ class ChatListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 10, // Example item count
+      itemCount: personList.length, // Example item count
       itemBuilder: (context, index) {
         return ListTile(
           leading: CircleAvatar(
             backgroundImage: AssetImage('assets/profle.jpeg'),
           ),
-          title: Text('John Doe'),
+          title: Text(personList[index]["name"]),
           subtitle: Text('Hey, how are you?'),
           trailing: Text('10:30 AM'),
           onTap: () {
