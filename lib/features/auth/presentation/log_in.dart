cbe9d3dbf725033a60e75d9ea7fd3638159f7575
Nadia Islam <sohasara853@gmail.com>
2025-07-14 17:28:41 +0600

    Add LogInPage widget for user authentication interface

---
+++ lib/features/auth/presentation/log_in.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LogInPage extends StatelessWidget {
+  const LogInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Log In')),
+      body: Center(child: Text('Log In Page')),
+    );
+  }
+}
