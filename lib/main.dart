0ea9a74158ce76b6d5937093ab5d80dc383377a4
Nadia Islam <sohasara853@gmail.com>
2025-07-18 12:10:29 +0600

    Remove firebase_auth and google_sign_in dependencies and related plugin registrations

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const ProviderScope(child: MyApp()));
 }
