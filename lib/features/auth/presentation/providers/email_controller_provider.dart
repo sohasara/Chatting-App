b41cf73408941dd9e2c83448e578cf82a23fc95c
Nadia Islam <sohasara853@gmail.com>
2025-08-14 11:01:55 +0600

    Add email and password controller providers with auto-dispose functionality

---
+++ lib/features/auth/presentation/providers/email_controller_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final emailControllerProvider = Provider.autoDispose<TextEditingController>((
+  ref,
+) {
+  final controller = TextEditingController();
+  ref.onDispose(() => controller.dispose());
+  return controller;
+});
